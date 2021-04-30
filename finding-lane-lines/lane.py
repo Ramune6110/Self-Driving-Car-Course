1b849a4e76e9e3c1945e15ba6974fb76ab98d949
Ramune6110 <arduino6110@gmail.com>
2021-04-30 02:06:16 -0700

    [modify]lane detect

--- finding-lane-lines/lane.py
+++ finding-lane-lines/lane.py
@@ -8,6 +8,23 @@ def canny(image):
     canny = cv2.Canny(blur, 50, 150)
     return canny
 
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+    return line_image
+
+"""
+def display_lines(img,lines):
+    line_image = np.zeros_like(img)
+    if lines is not None:
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
+    return line_image
+"""
 def region_of_interest(image):
     height = image.shape[0]
     polygones = np.array([
@@ -17,13 +34,19 @@ def region_of_interest(image):
     ])
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, polygones, 255)
-    return mask
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
     
 image = cv2.imread('figure/lane_image.jpg')
 lane_image = np.copy(image)
 canny = canny(lane_image)
-
-cv2.imshow("result", region_of_interest(canny))
+cropped_image = region_of_interest(canny)
+minLineLength = 40
+maxLineGap = 5
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi / 180, 100, np.array([]), minLineLength, maxLineGap)
+line_image = display_lines(lane_image, lines)
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+cv2.imshow("result", combo_image)
 cv2.waitKey(0)
 
 #plt.imshow(canny)
